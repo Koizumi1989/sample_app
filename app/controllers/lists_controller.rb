18883ddd2d9d91abf233d08e24a87a8ee8e88122
EC2 Default User <ec2-user@ip-172-31-21-229.ec2.internal>
2022-01-05 14:31:08 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,15 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+  # 「List.new」と定義することで、Listモデルの情報をもとに
+# 新しくオブジェクトが作成され、Listモデルに存在するtitle・bodyが格納
+# できるようになります。
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +19,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
